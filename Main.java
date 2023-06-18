be45a2841ba9dadc5f828db5f5c2f55667a53617
ff <ff@>
2023-06-18 19:45:12 +0200

    Przyklad - main

--- Main.java
+++ Main.java
@@ -1,48 +1,30 @@
 import Instrukcje.*;
 import Program.Program;
 import Wyrazenia.*;
+import Builder.*;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        Blok blok = new BlokBuilder()
+                .deklaracjaZmiennej("x", Literal.of(101))
+                .deklaracjaZmiennej("y", Literal.of(1))
+                .deklaracjaProcedury(new ProceduraBuilder("out", List.of("a"))
+                        .print(Dodawanie.dodaj(Zmienna.named("a"), Zmienna.named("x")))
+                        .build())
+                .przypisanie("x", Odejmowanie.odejmij(Zmienna.named("x"), Zmienna.named("y")))
+                .wykonajProcedure("out", List.of(Zmienna.named("x")))
+                .wykonajProcedure("out", List.of(Literal.of(100)))
+                .blok(new BlokBuilder()
+                        .deklaracjaZmiennej("x", Literal.of(10))
+                        .wykonajProcedure("out", List.of(Literal.of(100)))
+                        .build())
+                .build();
 
-        List<DeklaracjaZmiennej> d1 = new ArrayList<>();
-        d1.add(new DeklaracjaZmiennej("n", new Literal(30)));
-        Blok b1 = new Blok(d1);
-
-        Wyrazenie f1 = new Odejmowanie(new Zmienna("n"), new Literal(1));
-        Petla for1 = new Petla("k", f1);
-        b1.dodajInstrukcje(for1);
-
-        List<DeklaracjaZmiennej> d2 = new ArrayList<>();
-        d2.add(new DeklaracjaZmiennej("p", new Literal(1)));
-        Blok b2 = new Blok(d2);
-
-        b2.dodajInstrukcje(new Przypisanie("k", new Dodawanie(new Zmienna("k"), new Literal(2))));
-        for1.dodajInstrukcje(b2);
-
-        Wyrazenie f2 = new Odejmowanie(new Zmienna("k"), new Literal(2));
-        Petla for2 = new Petla("i", f2);
-        b2.dodajInstrukcje(for2);
-
-        for2.dodajInstrukcje(new Przypisanie("i", new Dodawanie(new Zmienna("i"), new Literal(2))));
-
-        Warunkowa war1 = new Warunkowa(new Modulo(new Zmienna("k"), new Zmienna("i")), new Literal(0), "=");
-        war1.dodajPrawda(new Przypisanie("p", new Literal(0)));
-
-        for2.dodajInstrukcje(war1);
-
-        Warunkowa war2 = new Warunkowa(new Zmienna("p"), new Literal(1), "=");
-        war2.dodajPrawda(new Print(new Zmienna("k")));
-
-
-        b2.dodajInstrukcje(war2);
-
-        Program program = new Program(b1);
-        program.wykonaj();
-
+        Program p = new Program(blok);
+        p.wykonaj();
     }
 }
